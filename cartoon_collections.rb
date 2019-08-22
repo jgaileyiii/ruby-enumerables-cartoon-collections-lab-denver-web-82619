21e880aaf0793420857dca1cc81c5751b1338a7a
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-22 18:54:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,3 +25,12 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 long_planeteer_calls(planeteer_calls)
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
+
+find_the_cheese(potentially_cheesy_items)
